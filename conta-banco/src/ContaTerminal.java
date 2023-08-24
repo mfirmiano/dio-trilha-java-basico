3a2f6c22d35f38f5f2a7b0ba9d057a014e08c034
mfirmiano <mfirmiano1902@gmail.com>
2023-08-24 05:10:56 -0300

    Cria o primeiro projeto em Java

--- conta-banco/src/ContaTerminal.java
+++ conta-banco/src/ContaTerminal.java
@@ -1,5 +1,33 @@
+import java.util.Locale;
+import java.util.Scanner;
+
 public class ContaTerminal {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
-    }
+        Scanner scanner = new Scanner(System.in).useLocale(Locale.US);
+
+        int numero;
+        String agencia, nomeCompleto, primeiroNome, segundoNome;
+        float saldo;
+
+        System.out.println("Por favor, insira o número da conta:");
+        numero = scanner.nextInt();
+
+        System.out.println("Agora digite a agência:");
+        agencia = scanner.next();
+
+        System.out.println("Informe o seu Primeiro Nome:");
+        primeiroNome = scanner.next();
+
+        System.out.println("Informe o seu Segundo Nome:");
+        segundoNome = scanner.next();
+
+        nomeCompleto = primeiroNome + " ".concat(segundoNome);
+
+        System.out.println("Informe o saldo:");
+        saldo = scanner.nextFloat();
+
+        String mensagem = "Olá ".concat(nomeCompleto).concat(", obrigado por criar uma conta em nosso banco. Sua agência é ").concat(agencia).concat(", conta ").concat(String.valueOf(numero)).concat(" e seu saldo ").concat(String.format("%.2f", saldo)).concat(" já está disponível para saque.");
+    
+        System.out.println(mensagem);
+    }   
 }
